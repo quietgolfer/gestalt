41b4960e47b4068055004d055bd8c4d3473daddb
Chris Lloyd <chrislloyd@pinterest.com>
2016-03-24 13:57:31 -0700

    Jest tests for TextField.
    Summary: This adds the ability for us to unit test React components with Jest. :O
    
    Test Plan:
    `npm run test`
    
    Also ran `jest --coverage` to help build up the initial set of tests. Coverage is at like, 89% currently which isn't ideal but the component could do with some design love too.

---
+++ components/TextField/__tests__/TextField-test.js
@@ -0,0 +1,68 @@
+/* eslint-env jest */
+jest.unmock('../TextField');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import TextField from '../TextField';
+
+describe('<TextField />', () => {
+    it('is controlled', () => {
+        const textField = TestUtils.renderIntoDocument(
+            <TextField value="Text" />
+        );
+        expect(textField.getValue()).toBe('Text');
+    });
+
+    it('is uncontrolled', () => {
+        const textField = TestUtils.renderIntoDocument(
+            <TextField />
+        );
+        const el = ReactDOM.findDOMNode(textField);
+        el.value = 'Text';
+        TestUtils.Simulate.change(el);
+        // While this assertion might seem… not useful if the component is
+        // controlled, the change event will change the value of the element
+        expect(el.value).toBe('Text');
+    });
+
+    it('is focusable', () => {
+        const textField = TestUtils.renderIntoDocument(
+            <TextField focused />
+        );
+        const el = ReactDOM.findDOMNode(textField);
+        expect(document.activeElement).toBe(el);
+    });
+
+    it('validates input', () => {
+        let errors = false;
+        const textField = TestUtils.renderIntoDocument(
+            <TextField
+                validators={[() => ({errors: ['error']})]}
+                onValidation={() => errors = true} />
+        );
+        const el = ReactDOM.findDOMNode(textField);
+        expect(!errors);
+        expect(el.className).toBe('');
+        textField.text = 'Text';
+        TestUtils.Simulate.change(el);
+        expect(errors);
+    });
+
+    it('triggers onChange callback', () => {
+        let changed = false;
+        const textField = TestUtils.renderIntoDocument(
+            <TextField onChange={() => changed = true} />
+        );
+        const el = ReactDOM.findDOMNode(textField);
+        TestUtils.Simulate.change(el);
+        expect(changed);
+    });
+
+    it('getId returns the id passed in as a prop', () => {
+        const textField = TestUtils.renderIntoDocument(
+            <TextField id="TextField 123" />
+        );
+        expect(textField.getId()).toBe("TextField 123");
+    });
+});
