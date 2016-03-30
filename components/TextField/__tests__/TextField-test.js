73170a4cd79e102761f25e050b5222ae4bf5cbcf
Chris Lloyd <chrislloyd@pinterest.com>
2016-03-29 17:11:11 -0700

    Refactor and simplify TextField implementation
    Summary: I wanted to make the TextField component a far simpler wrapper over just a regular React input. Major change is removing the validation stuff - that's application specific logic that should probably live higher up in the component tree.

--- components/TextField/__tests__/TextField-test.js
+++ components/TextField/__tests__/TextField-test.js
@@ -7,16 +7,16 @@ import TestUtils from 'react-addons-test-utils';
 import TextField from '../TextField';
 
 describe('<TextField />', () => {
-    it('is controlled', () => {
+    it('can be controlled', () => {
         const textField = TestUtils.renderIntoDocument(
-            <TextField value="Text" />
+            <TextField onChange={() => {}} value="Text" />
         );
-        expect(textField.getValue()).toBe('Text');
+        expect(textField.value).toBe('Text');
     });
 
-    it('is uncontrolled', () => {
+    it('can be uncontrolled', () => {
         const textField = TestUtils.renderIntoDocument(
-            <TextField />
+            <TextField defaultValue="DefaultText" />
         );
         const el = ReactDOM.findDOMNode(textField);
         el.value = 'Text';
@@ -24,45 +24,5 @@ describe('<TextField />', () => {
         // While this assertion might seem… not useful if the component is
         // controlled, the change event will change the value of the element
         expect(el.value).toBe('Text');
-    });
-
-    it('is focusable', () => {
-        const textField = TestUtils.renderIntoDocument(
-            <TextField focused />
-        );
-        const el = ReactDOM.findDOMNode(textField);
-        expect(document.activeElement).toBe(el);
-    });
-
-    it('validates input', () => {
-        let errors = false;
-        const textField = TestUtils.renderIntoDocument(
-            <TextField
-                onValidation={() => errors = true}
-                validators={[() => ({errors: ['error']})]} />
-        );
-        const el = ReactDOM.findDOMNode(textField);
-        expect(!errors);
-        expect(el.className).toBe('');
-        textField.text = 'Text';
-        TestUtils.Simulate.change(el);
-        expect(errors);
-    });
-
-    it('triggers onChange callback', () => {
-        let changed = false;
-        const textField = TestUtils.renderIntoDocument(
-            <TextField onChange={() => changed = true} />
-        );
-        const el = ReactDOM.findDOMNode(textField);
-        TestUtils.Simulate.change(el);
-        expect(changed);
-    });
-
-    it('getId returns the id passed in as a prop', () => {
-        const textField = TestUtils.renderIntoDocument(
-            <TextField id="TextField 123" />
-        );
-        expect(textField.getId()).toBe("TextField 123");
     });
 });
