c0d35139f2f7c9d31431d83db945de69e6c8e882
Chris Lloyd <chrislloyd@users.noreply.github.com>
2016-05-12 16:31:08 -0700

    Bowser build (#8)
    Updates built CSS to be `!important` and prefixed so that we can use
    this as a stepping stone to incorporating Gestalt 100%. It’s ugly, and
    please don’t try this at home unless you are migrating large codebases.

--- webpack.config.js
+++ webpack.config.js
@@ -12,30 +12,33 @@ module.exports = {
         loaders: [
             {
                 test: /\.css$/,
+                include: /src/,
                 loader: ExtractTextPlugin.extract(
                     'style',
-                    [
-                        'css?modules&importLoaders=1&localIdentName=[local]',
-                        'postcss'
-                    ],
-                    {filename: 'gestalt.css'}
+                    'css?modules&importLoaders=1&localIdentName=[local]!postcss'
                 )
             }
         ],
     },
     postcss: function(webpack) {
         return [
-            require('postcss-import')({addDependencyTo: webpack}),
+            require('postcss-import')({
+                addDependencyTo: webpack
+            }),
             require('postcss-cssnext')({
                 features: {
                     customMedia: {
                         extensions: require('./src/breakpoints')
                     }
                 }
-            })
+            }),
+            require('postcss-importantly')(),
+            require('postcss-class-prefix')('dp-')
         ];
     },
     plugins: [
-        new ExtractTextPlugin('gestalt.css'),
+        new ExtractTextPlugin('[name].css', {
+            allChunks: true
+        }),
     ]
 };
