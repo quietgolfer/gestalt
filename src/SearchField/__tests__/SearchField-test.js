514094e7d02256e449e2864777f0142003eda53c
Kevin Grandon <kevingrandon@yahoo.com>
2016-05-13 11:28:48 -0700

    Use enzyme instead of TestUtils. (#10)

--- src/SearchField/__tests__/SearchField-test.js
+++ src/SearchField/__tests__/SearchField-test.js
@@ -1,28 +1,30 @@
 /* eslint-env jest */
 jest.unmock('../SearchField');
 
+import {shallow} from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import SearchField from '../SearchField';
 
 describe('<SearchField />', () => {
     it('can be controlled', () => {
-        const textField = TestUtils.renderIntoDocument(
+        const searchField = shallow(
             <SearchField onChange={() => {}} value="Text" />
         );
-        expect(textField.value).toBe('Text');
+        const input = searchField.find('input');
+        expect(input.prop('value')).toBe('Text');
     });
 
     it('can be uncontrolled', () => {
-        const textField = TestUtils.renderIntoDocument(
+        const searchField = shallow(
             <SearchField defaultValue="DefaultText" />
         );
-        const el = ReactDOM.findDOMNode(textField);
-        el.value = 'Text';
-        TestUtils.Simulate.change(el);
+        const input = searchField.find('input');
+        expect(input.prop('value')).toBe(undefined);
+        expect(input.prop('defaultValue')).toBe('DefaultText');
+
+        searchField.simulate('change');
         // While this assertion might seem… not useful if the component is
         // controlled, the change event will change the value of the element
-        expect(el.value).toBe('Text');
+        expect(input.prop('value')).toBe(undefined);
     });
 });
--- src/TextField/__tests__/TextField-test.js
+++ src/TextField/__tests__/TextField-test.js
@@ -1,28 +1,29 @@
 /* eslint-env jest */
 jest.unmock('../TextField');
 
+import {shallow} from 'enzyme';
 import React from 'react';
-import ReactDOM from 'react-dom';
-import TestUtils from 'react-addons-test-utils';
 import TextField from '../TextField';
 
 describe('<TextField />', () => {
     it('can be controlled', () => {
-        const textField = TestUtils.renderIntoDocument(
+        const textField = shallow(
             <TextField onChange={() => {}} value="Text" />
         );
-        expect(textField.value).toBe('Text');
+        expect(textField.prop('value')).toBe('Text');
     });
 
     it('can be uncontrolled', () => {
-        const textField = TestUtils.renderIntoDocument(
+        const textField = shallow(
             <TextField defaultValue="DefaultText" />
         );
-        const el = ReactDOM.findDOMNode(textField);
-        el.value = 'Text';
-        TestUtils.Simulate.change(el);
+        const input = textField.find('input');
+        expect(input.prop('value')).toBe(undefined);
+        expect(input.prop('defaultValue')).toBe('DefaultText');
+
+        input.simulate('change');
         // While this assertion might seem… not useful if the component is
         // controlled, the change event will change the value of the element
-        expect(el.value).toBe('Text');
+        expect(input.prop('value')).toBe(undefined);
     });
 });
