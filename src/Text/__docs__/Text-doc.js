c96bd25cd2e9a0fb89e96aa02cd1511c9c11948f
Kevin Grandon <kevingrandon@yahoo.com>
2016-08-16 10:25:34 -0700

    Add Japanese examples in text component. (#70)
    This will ensure that we consider localization when iterating on components.

--- src/Text/__docs__/Text-doc.js
+++ src/Text/__docs__/Text-doc.js
@@ -7,9 +7,29 @@ ns('Text');
 card('Text',
   doc`Pretty standard, really.`,
   <div>
-    <Text size="xs">{'Text extra small'}</Text>
-    <Text size="s">{'Text small'}</Text>
-    <Text size="m">{'Text medium'}</Text>
-    <Text size="l">{'Text large'}</Text>
-    <Text size="xl">{'Text extra large'}</Text>
+    <p>
+      <Text size="xs">{'Text extra small'}</Text>
+      {' '}
+      <Text size="xs">{'こんにちは'}</Text>
+    </p>
+    <p>
+      <Text size="s">{'Text small'}</Text>
+      {' '}
+      <Text size="s">{'こんにちは'}</Text>
+    </p>
+    <p>
+      <Text size="m">{'Text medium'}</Text>
+      {' '}
+      <Text size="m">{'こんにちは'}</Text>
+    </p>
+    <p>
+      <Text size="l">{'Text large'}</Text>
+      {' '}
+      <Text size="l">{'こんにちは'}</Text>
+    </p>
+    <p>
+      <Text size="xl">{'Text extra large'}</Text>
+      {' '}
+      <Text size="xl">{'こんにちは'}</Text>
+    </p>
   </div>);
