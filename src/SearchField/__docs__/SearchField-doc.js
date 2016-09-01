1b4202d22cbf82413fed2c88ffd5e29c26b7b12d
Christian Vuerings <vueringschristian@gmail.com>
2016-09-01 10:50:48 -0700

    Removes TextField, SearchField, Navbar & Modal (#110)

--- src/SearchField/__docs__/SearchField-doc.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import SearchField from '../SearchField';
-import { card, md, ns } from 'corkboard';
-
-ns('SearchField');
-
-card('SearchField',
-  md`Used anywhere we offer search or some type-ahead like functionality.`,
-  <SearchField readOnly value="Foo" />);
--- src/TextField/__docs__/TextField-doc.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import TextField from '../TextField';
-import { card, md, ns } from 'corkboard';
-
-ns('TextField');
-
-card('TextField',
-  md`Lorem ipsum dolor sit amet.`,
-  <TextField readOnly value="Foo" />);
