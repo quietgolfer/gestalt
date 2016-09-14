5eef76b10c5781fd43def1cdd7ec74a9c096300c
Kevin Grandon <kevingrandon@yahoo.com>
2016-09-13 17:34:03 -0700

    Update gh-pages deploy script. (#146)
    * Update gh-pages deploy script.
    
    * Use full repo path.

--- scripts/ghpages.sh
+++ scripts/ghpages.sh
@@ -11,10 +11,10 @@ set -x
 
 git checkout -b tmp-deploy
 npm run build_docs
-git add docs
+git add -f docs
 git commit -m "Deployed to Github Pages"
-git push origin :gh-pages
-git subtree push --prefix docs origin gh-pages
+git push git@github.com:pinterest/gestalt.git :gh-pages
+git subtree push --prefix docs git@github.com:pinterest/gestalt.git gh-pages
 git checkout master
 git reset --hard HEAD
 git branch -D tmp-deploy
