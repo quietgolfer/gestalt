e92c0595e0ac75e67c872484bcf74a711f9ecea6
Julia Cochran <juliacochran7@gmail.com>
2016-09-16 17:24:17 -0700

    add proptypes to grid docs (#160)

--- src/ClassicGrid/__docs__/ClassicGrid-doc.js
+++ src/ClassicGrid/__docs__/ClassicGrid-doc.js
@@ -6,6 +6,22 @@ import { ns } from '../../../.corkboard/cards';
 
 ns('ClassicGrid');
 
+card('PropTypes',
+  md`
+  \`\`\`js
+  ClassicGrid.propTypes = {
+    columnWidth: React.PropTypes.number, /* default: 236 */
+    comp: React.PropTypes.func, /* the component to render */
+    gutterWidth: React.PropTypes.number, /* default: 14 */
+    items: React.PropTypes.array, /* objects to display in grid */
+    loadItems: React.PropTypes.func, /* callback when user scrolls and we need to load more items */
+    minCols: React.PropTypes.number, /* default : 3 */
+    scrollContainer: React.PropTypes.object, /* default: window */
+  };
+  \`\`\`
+  `
+  );
+
 card('Fixed width',
   md`With a set number of items, used as a collage component.`,
   <div style={{ width: 400 }}>
--- src/FlexibleGrid/__docs__/FlexibleGrid-doc.js
+++ src/FlexibleGrid/__docs__/FlexibleGrid-doc.js
@@ -6,6 +6,20 @@ import { ns } from '../../../.corkboard/cards';
 
 ns('FlexibleGrid');
 
+card('PropTypes',
+  md`
+  \`\`\`js
+  FlexibleGrid.propTypes = {
+    comp: React.PropTypes.func, /* component to render */
+    items: React.PropTypes.array, /* objects to display in grid */
+    loadItems: React.PropTypes.func, /* callback when user scrolls and we need to load more items */
+    maxItemWidth: React.PropTypes.number, /* default: 300 */
+    minItemWidth: React.PropTypes.number, /* default: 236 */
+    scrollContainer: React.PropTypes.object, /* default: window */
+  };
+  \`\`\`
+  `);
+
 card('Fixed width container',
   md`With a set number of items, used as a collage component.`,
   <div style={{ width: 400 }}>
