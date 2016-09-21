eff2e0a09e6c59e341bcb7f06a384e1651d7cdb6
Christian Vuerings <vueringschristian@gmail.com>
2016-09-20 19:17:14 -0700

    Initial version of the IconButton component (#172)
    * Initial version of the RoundedButton component
    
    Couple of things we might want to add in the future:
    `disabled`, `color` and `size`
    
    * Rename RoundedButton to IconButton
    
    * Don't export IconButton yet
    
    * Change required to isRequired
    
    * Make icon label the same as the button label
    
    * Keep label empty but add a comment in the code

---
+++ src/IconButton/__docs__/IconButton-doc.js
@@ -0,0 +1,60 @@
+// @flow
+import React, { PropTypes } from 'react';
+import IconButton from '../IconButton';
+import { card, md } from 'corkboard';
+import { ns } from '../../../.corkboard/cards';
+
+ns('IconButton', `
+  The IconButton component allows you to
+  define an action with an \`Icon\`.
+\`\`\`js
+IconButton.propTypes = {
+  icon: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+};
+\`\`\`
+`);
+
+const iconButtonExamples = [
+  'add',
+  'arrow-back',
+  'arrow-forward',
+  'cancel',
+  'cog',
+  'edit',
+  'ellipsis',
+  'heart',
+  'send',
+  'share',
+];
+
+function IconButtonExample({ iconName }) {
+  return (
+    <div className="col-6 sm-col-3 px2 mb1 text-center border-box">
+      <div className="mb1">
+        <strong>{iconName}</strong>
+      </div>
+      <IconButton icon={iconName} label={iconName.replace(/-/g, ' ')} />
+    </div>
+  );
+}
+
+IconButtonExample.propTypes = {
+  iconName: PropTypes.string.isRequired,
+};
+
+card('Icons',
+md`You can use any icon defined in the <a href="#/Icon">Icon component</a>.
+\`\`\`html
+<IconButton
+  icon="pin"
+  label="Pin"
+/>
+\`\`\`
+`,
+  <div className="flex mxn2 flex-wrap">
+    {iconButtonExamples.map((icon, idx) =>
+      <IconButtonExample iconName={icon} key={idx} />
+    )}
+  </div>);
